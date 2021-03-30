35a834c652cf049e5056b142bcee1e364c4ad6a2
Peter Lehnhardt <aliud@posteo.at>
2021-03-30 17:38:10 +0200

    Handle 404

--- backend/src/scraping/apod_data/mod.rs
+++ backend/src/scraping/apod_data/mod.rs
@@ -2,27 +2,26 @@ mod getter;
 mod normalization;
 mod translation;
 
+use super::error::ScrapeResult;
 use crate::apod::APOD;
 use crate::APODRequestClient;
 use getter::{get_description, get_img_url, get_meta, get_title};
-use super::error::ScrapeResult;
 
 pub async fn get_apod_data(date: &str, client: &APODRequestClient) -> ScrapeResult<Option<APOD>> {
   let year = &date[2..4];
   let month = &date[5..7];
   let day = &date[8..10];
   let url = format!("https://apod.nasa.gov/apod/ap{}{}{}.html", year, month, day);
-  let sub_page = client
-    .get(&url)
-    .await
-    .text()
-    .await
-    .expect("Could not get text");
+  let page_response = client.get(&url).await;
+  if page_response.status().as_u16() == 404 {
+    return Ok(None);
+  }
+  let page = page_response.text().await.expect("Could not get text");
 
-  let description = get_description(&sub_page);
-  let img_url = get_img_url(&sub_page);
-  let title = get_title(&sub_page);
-  let meta = get_meta(&sub_page);
+  let description = get_description(&page);
+  let img_url = get_img_url(&page);
+  let title = get_title(&page);
+  let meta = get_meta(&page);
 
   Ok(Some(APOD {
     id: None,
