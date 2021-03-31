7dca20b6bae13d5d8eb8a7c5f022349aa2552b0e
Peter Lehnhardt <aliud@posteo.at>
2021-03-31 17:34:09 +0200

    Improve relative url detection

--- backend/src/scraping/apod_data/normalization/url/mod.rs
+++ backend/src/scraping/apod_data/normalization/url/mod.rs
@@ -1,6 +1,9 @@
-use regex::Regex;
+mod check;
+
+use check::is_rel_website_url;
 
 pub fn normalize_url(url: &str) -> String {
+  // TODO move fixes here to own module
   let url_without_invalid_chars = url
     .replace("\n", "")
     .replace(" ", "")
@@ -15,18 +18,10 @@ pub fn normalize_url(url: &str) -> String {
       .replace("[dot]", ".")
       .replace(".d.o.t.", ".")
   } else {
-    let top_level_domain = r"[a-zA-Z]{2,}";
-    let domain_label = r"(?:[a-zA-Z0-9][a-zA-Z0-9\-]+?[a-zA-Z0-9]|[a-zA-Z0-9]{1,2})";
-    let valid_full_url_regex = format!(
-      r"^(?:https?://)?(?:{}\.)+{}",
-      domain_label, top_level_domain
-    );
-    match Regex::new(valid_full_url_regex.as_str())
-      .unwrap()
-      .is_match(&url_without_invalid_chars)
-    {
-      true => String::from(url_without_invalid_chars),
-      false => format!("https://apod.nasa.gov/apod/{}", url_without_invalid_chars),
+    if is_rel_website_url(&url_without_invalid_chars) {
+      format!("https://apod.nasa.gov/apod/{}", url_without_invalid_chars)
+    } else {
+      url_without_invalid_chars
     }
   }
 }
@@ -69,14 +64,6 @@ mod tests {
       normalize_url("http://www.google.de<"),
       "http://www.google.de"
     );
-  }
-
-  #[test]
-  fn changes_relative_to_absolute_url() {
-    assert_eq!(
-      normalize_url("images/test.jpeg"),
-      "https://apod.nasa.gov/apod/images/test.jpeg"
-    )
   }
 
   #[test]
