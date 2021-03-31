de08fbabbe56cddcdfa596f21cc77e7b0d840d32
Peter Lehnhardt <aliud@posteo.at>
2021-03-31 17:35:12 +0200

    Remove fix bypasses

--- backend/src/scraping/apod_data/normalization/text/fix/html_tag/mod.rs
+++ backend/src/scraping/apod_data/normalization/text/fix/html_tag/mod.rs
@@ -1,17 +1,11 @@
-mod check;
 mod detect;
 mod fix;
 
 use crate::scraping::ScrapeResult;
-use check::tag_syntax_is_valid;
 use detect::detect_tag;
 use fix::fix_tag;
 
 pub fn normalize_html_tag(tag: &str) -> ScrapeResult<String> {
-  if tag_syntax_is_valid(tag) {
-    return Ok(String::from(tag));
-  }
-
   let tag_type = detect_tag(tag)?;
   Ok(fix_tag(tag, &tag_type))
 }
