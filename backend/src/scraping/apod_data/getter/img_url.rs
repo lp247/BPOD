998f5aba581803b2a64fbc85a760630530c96cbc
Peter Lehnhardt <aliud@posteo.at>
2021-04-03 12:51:06 +0200

    Add source location

--- backend/src/scraping/apod_data/getter/img_url.rs
+++ backend/src/scraping/apod_data/getter/img_url.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 pub fn get_img_url(page: &str) -> String {
     // TODO: Get image source not from image tag but from enclosing link
     let regex =
-    Regex::new(r#"<(?:IMG[\s\S]+?SRC|img[\s\S]+?src|iframe[\s\S]+?src|object[\s\S]+?data)=["'](?P<url>.+?)["']"#)
+    Regex::new(r#"<(?:IMG[\s\S]+?SRC|img[\s\S]+?src|iframe[\s\S]+?src|object[\s\S]+?data|param name="movie" value)=["'](?P<url>.+?)["']"#)
       .unwrap();
     let captures = regex.captures(page).expect("Could not find image source");
     let url = captures
